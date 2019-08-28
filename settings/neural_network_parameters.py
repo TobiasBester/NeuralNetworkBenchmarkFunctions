e8e405336c5f975c9610dfe860f17dfbeba4281a
Tobias <tobias@labs.epiuse.com>
2019-08-28 18:26:43 +0200

    Moved TODOs and changed some parameters

--- settings/neural_network_parameters.py
+++ settings/neural_network_parameters.py
@@ -26,13 +26,3 @@ class NeuralNetworkParameters:
             return optimizers.sgd_optimizer()
         if optimizer == 'Adam':
             return optimizers.adam_optimizer()
-
-# TODO: a) Increase number of hidden neurons
-# TODO: a) Change Compare runner to use updated Runners
-# TODO: a) Normalize f(x, y) range to [0, 1] or [-10, 10] or whatever
-# TODO: b) Investigate Linear Regression from scikit-learn
-# TODO: b) Set different seeds for neural network runs (up to 10 and then use the Mean MSE)
-# TODO: c) Look for more functions
-# TODO: d) Set NNMSE/LRMSE to something that falls at 0 if equal (in Excel)
-# TODO: d) Save output to Latex table
-# TODO: e) Plot true surface against NN predicted surface
