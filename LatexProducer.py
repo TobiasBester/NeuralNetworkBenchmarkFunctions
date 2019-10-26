1f8a59156f2ba2bc53955dd65a64eec448121f4a
Tobias <tbester23@gmail.com>
2019-10-26 18:21:16 +0200

    Add result plots
    - Change LatexProducer to round values to the right precision
    - Add all true function plots, neural network plots, and LR plots

--- LatexProducer.py
+++ LatexProducer.py
@@ -6,6 +6,20 @@ def remove_newline(data):
         x[-1] = x[-1][:-2]
 
 
+def ensure_precision(data):
+    epochs_precision = 2
+    mse_precision = 6
+    stdev_precision = 4
+    mse_index_precision = 6
+
+    for x in data:
+        x[1] = round(float(x[1]), epochs_precision)
+        x[2] = round(float(x[2]), mse_precision)
+        x[3] = round(float(x[3]), stdev_precision)
+        x[4] = round(float(x[4]), mse_precision)
+        x[5] = round(float(x[5]), mse_index_precision)
+
+
 def data_to_dict(data):
     return {x[0]: x[1:] for x in data}
 
@@ -27,6 +41,7 @@ def latex():
     data = [x.split('|') for x in f.readlines()]
 
     remove_newline(data)
+    ensure_precision(data)
     data = data_to_dict(data)
     data = sort_dict_keys(data)
     data = dict_to_list(data)
